60a338c1ad1b39b99847eb7695fe99305a4edb57
lightjia <lightjia@163.com>
2019-11-08 16:20:55 +0800

    Add MergeSort

--- src/jlalgorithm/jlsort.go
+++ src/jlalgorithm/jlsort.go
@@ -37,3 +37,33 @@ func Jl_quicksort(array []int, start, end int) {
 	Jl_quicksort(array[:], imid, istart) //递归进行左侧排序
 	Jl_quicksort(array[:], istart, end)  //递归进行右侧排序
 }
+
+func jl_merge(arr1, arr2 []int) (r []int) {
+	r = make([]int, len(arr1)+len(arr2))
+	i, i1, i2 := 0, 0, 0
+	for ; i < len(r); i++ {
+		if i1 < len(arr1) {
+			if (i2 < len(arr2) && arr1[i1] < arr2[i2]) || i2>=len(arr2) {
+				r[i] = arr1[i1]
+				i1++
+				continue
+			}
+		}
+
+		r[i] = arr2[i2]
+		i2++
+	}
+
+	return r
+}
+
+func Jl_MergeSort(array []int) []int {
+	if (len(array) >= 2) {
+		r1 := Jl_MergeSort(array[:len(array)/2])
+		r2 := Jl_MergeSort(array[len(array)/2:])
+
+		return jl_merge(r1, r2)
+	}
+
+	return array
+}
--- src/main.go
+++ src/main.go
@@ -1,6 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"jlalgorithm"
+	"math/rand"
+)
+
 func main() {
+	const arraylen = 0xFF
+	array := make([]int, arraylen)
+	rand.Seed(99)
+	for i := 0; i < arraylen; i++ {
+		array[i] = rand.Intn(99)
+	}
 
-
+	fmt.Println("before sort", array)
+	tmp := jlalgorithm.Jl_MergeSort(array)
+	fmt.Println("after sort", tmp)
 }
